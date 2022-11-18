d700c3797a18f922cda28a0e92d6303a3b47d47e
Ahmed Abdelhuy <ahmelhuy@gmail.com>
2022-11-18 22:15:13 +0200

    add data base initializer

---
+++ src/database.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const { 
+  POSTGRES_HOST, 
+  POSTGRES_DEV_DB,
+  POSTGRES_TEST_DB,
+  POSTGRES_USER, 
+  POSTGRES_PASSWORD,
+  ENV
+} = process.env;
+
+const Client = new Pool({
+  host: POSTGRES_HOST,
+  database: (ENV === "test" ?
+               POSTGRES_TEST_DB : POSTGRES_DEV_DB),
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+});
+
+export default Client;

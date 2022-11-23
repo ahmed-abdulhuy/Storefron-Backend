07175c97563202b46f95b2d808b1ca5ed2303a35
Ahmed Abdelhuy <ahmelhuy@gmail.com>
2022-11-23 23:04:31 +0200

    change database migration files

--- migrations/20221123161827-products-table.js
+++ migrations/20221123161827-products-table.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20221116202658-users-table-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20221123161827-products-table-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20221116202658-users-table-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20221123161827-products-table-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);

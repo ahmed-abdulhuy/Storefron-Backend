d258c1fb33fa1e18b0a5f095889b3c65466a7cf1
Ahmed Abdelhuy <ahmelhuy@gmail.com>
2022-11-25 14:31:00 +0200

    add supertests for user routes

--- src/models/tests/users_spec.ts
+++ src/models/tests/users_spec.ts
@@ -1,8 +1,12 @@
 import { UserStore } from "../users";
+import app from "../../server";
+import supertest from "supertest"
 
 const store = new UserStore();
+const request = supertest(app);
 
-describe("Users Model Tests", () => {
+
+describe("Users Model Test Methods Existance", () => {
   it("should have index method", () => {
     expect(store.index).toBeDefined();
   });
@@ -13,3 +17,19 @@ describe("Users Model Tests", () => {
     expect(store.create).toBeDefined();
   });
 });
+
+describe('User Handlers Response', () => {
+  it("Post request to create user should return JWT as string", async () => {
+    const query = {
+      firstName: "Naruto",
+      lastName: "Uzumaki",
+      password: "rasingan"
+    }
+    const response = await request.post("/users")
+                            .set('Content-type', 'application/json')
+                            .send({query})
+    
+    expect(response.status).toBe(200);
+    // expect(response.body)
+  })
+})
